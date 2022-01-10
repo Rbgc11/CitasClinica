f70560f3b596d7ed8c9956b6068c9b676254bb86
ruben <ruben@DESKTOP-JO9KI7I>
2022-01-10 12:28:35 +0100

    Ejercicio 4

---
+++ src/main/java/org/iesalandalus/programacion/citasclinica/modelo/Cita.java
@@ -0,0 +1,84 @@
+package org.iesalandalus.programacion.citasclinica.modelo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Cita {
+
+	// 4.1 Atributos
+
+	public final static String FORMATO_FECHA_HORA = "dd/MM/yyyy HH:mm";
+	private LocalDateTime fechaHora;
+	private Paciente paciente;
+
+	// 4.3 Constructor con parámetros
+
+	public Cita(Paciente paciente, LocalDateTime fechaHora) {
+		setPaciente(paciente);
+		setFechaHora(fechaHora);
+	}
+	// 4.4 Constructor copia
+
+	public Cita(Cita cita) {
+		if (cita == null) {
+			throw new NullPointerException("ERROR: No se puede copiar una cita nula.");
+		}
+		setPaciente(cita.getPaciente());
+		setFechaHora(cita.getFechaHora());
+	}
+	
+	// 4.2 Métodos Get y Set
+	private void setPaciente(Paciente paciente) {
+		if (paciente == null) {
+			throw new NullPointerException("ERROR: El paciente de una cita no puede ser nulo.");
+		}
+		this.paciente = new Paciente(paciente);
+	}
+	
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+
+	public void setFechaHora(LocalDateTime fechaHora) {
+		if (fechaHora == null) {
+			throw new NullPointerException("ERROR: La fecha y hora de una cita no puede ser nula.");
+		}
+		this.fechaHora = fechaHora;
+	}
+	
+	public LocalDateTime getFechaHora() {
+		return fechaHora;
+	}
+
+	// 4.5 Métodos equals y hashCode
+	@Override
+	public int hashCode() {
+		return Objects.hash(fechaHora, paciente);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cita other = (Cita) obj;
+		return Objects.equals(fechaHora, other.fechaHora) && Objects.equals(paciente, other.paciente);
+	}
+	
+
+	// 4.6 Método toString
+
+	@Override
+	public String toString() {
+		return String.format("%s, fechaHora=%s", paciente.toString(),
+				fechaHora.format(DateTimeFormatter.ofPattern(FORMATO_FECHA_HORA)));
+	}
+
+
+
+}
